877b3408d04d3c2a003427358f3aff9137af2993
mevlt01001 <mevlutbasaran01@gmail.com>
2026-07-11 16:35:57 +0300

    Added efficient batch feature to FeatureExtractor

--- utils/VideoFeatureExtractor.py
+++ utils/VideoFeatureExtractor.py
@@ -4,95 +4,98 @@ from tqdm import tqdm
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-# from .c3d import C3D
 from .video_preprocess import fetch_video_patches
 from torchvision.models.video import r2plus1d_18, R2Plus1D_18_Weights
 
-class C3D_FeatureExtractor(nn.Module):
+class FeatureExtractor(nn.Module):
     def __init__(self, clip_size: int = 16, overlap: int = 0):
-        super(C3D_FeatureExtractor, self).__init__()
+        super(FeatureExtractor, self).__init__()
         self.clip_size = clip_size
         self.overlap = overlap
         self.stride = clip_size - overlap
 
-        # 1. Pytorch'un Kinetics-400 ile eğitilmiş yerleşik modelini yüklüyoruz
         weights = R2Plus1D_18_Weights.DEFAULT
         self.backbone = r2plus1d_18(weights=weights)
-
-        # 2. Sınıflandırma (FC) katmanını kesip atıyoruz, sadece 512 boyutlu özellikleri istiyoruz
         self.backbone.fc = nn.Identity()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        x.shape: (B, C, S, H, W)
-        R(2+1)D modeli 112x112 piksel boyutlarında harika çalışır.
-        """
+
         B, C, S, H, W = x.shape
 
-        # Padding (Eğer video kare sayısı clip_size'dan küçükse 0 ile doldur)
         pad_size = max(0, self.clip_size - S)
         if pad_size > 0:
             x = F.pad(x, (0, 0, 0, 0, 0, pad_size))
             S = x.shape[2]
 
-        # Videoyu 16 karelik alt kliplere ayırma
         x_unfolded = x.unfold(2, self.clip_size, self.stride)
         x_clips = x_unfolded.permute(0, 2, 1, 5, 3, 4)
         num_clips = x_clips.shape[1]
 
-        # Modele göndermek için batch ve clip boyutlarını birleştir
-        h = x_clips.reshape(-1, C, self.clip_size, H, W)
-
-        # R(2+1)D ile özellikleri çıkar (Çıktı: B*num_clips, 512)
+        h = x_clips.reshape(-1, C, self.clip_size, H, W)    # [B, 3, S, H, W]
         h = self.backbone(h)
-
-        # Vektörleri normalize et (MIL ağının dengeli öğrenmesi için hayati önem taşır)
         h = F.normalize(h, p=2, dim=-1)
-
-        # Kliplerin ortalamasını alarak tüm video segmenti için tek vektör oluştur
         h = h.reshape(B, num_clips, -1)
-        h = h.mean(dim=1) # Son Çıktı: (B, 512)
+        h = h.mean(dim=1)   # [B, 512]
 
         return h
-
-# class C3D_FeatureExtractor(nn.Module):
-
-#     def __init__(self, pt_file: str, clip_size: int = 16, overlap: int = 0):
-#         super(C3D_FeatureExtractor, self).__init__()
-#         self.clip_size = clip_size
-#         self.overlap = overlap
-#         self.stride = clip_size - overlap
-
-#         self.C3D = C3D(n_classes=487)
-#         state_dict = torch.load(pt_file, map_location="cpu")
-#         self.C3D.load_state_dict(state_dict)
-
-#     def forward(self, x: torch.Tensor) -> torch.Tensor:
-#         """
-#         x.shape: (B, C, S, H, W), H=W=112 
-#         """
-#         B, C, S, H, W = x.shape
-
-#         pad_size = max(0, self.clip_size - S)
-#         x = F.pad(x, (0, 0, 0, 0, 0, pad_size))
-#         S = x.shape[2]
-
-#         x_unfolded = x.unfold(2, self.clip_size, self.stride)
-#         x_clips = x_unfolded.permute(0, 2, 1, 5, 3, 4)
-#         num_clips = x_clips.shape[1]
-
-#         h = x_clips.reshape(-1, C, self.clip_size, H, W)
-
-#         h = self.C3D.extract(h, layer="fc6")
+    
+    @torch.no_grad()
+    def extract_feats(self,
+                      video_path: os.PathLike, 
+                      patch_size: int, 
+                      fps: int, 
+                      resolution: tuple[int, int],
+                      safe_vram_mb: int = 3200):
         
-#         h = F.normalize(h, p=2, dim=-1)
+        torch.cuda.reset_peak_memory_stats()
+        initial_mem = torch.cuda.memory_allocated()
+        dummy = torch.randn(1, 3, self.clip_size, *resolution, device="cuda", dtype=torch.float32)
+        with torch.amp.autocast(device_type="cuda", dtype=torch.float16): _ = self.forward(dummy)
+        peak_mem = torch.cuda.max_memory_allocated()
+        bytes_per_clip = peak_mem - initial_mem
+        
+        del dummy, _
+        torch.cuda.empty_cache()
+        
+        length_of_clip = _probe_segment_length(video_path, fps, patch_size)
+        number_of_clip = _num_clips_for_segment(length_of_clip, self.clip_size, self.stride)
+        bytes_per_segment = bytes_per_clip * number_of_clip
+        
+        safe_vram_bytes = safe_vram_mb * 1024 * 1024
+        batch_size = max(1, int(safe_vram_bytes // bytes_per_segment))
 
-#         h = h.reshape(B, num_clips, -1)
-#         h = h.mean(dim=1)
+        patches = [patch for patch in fetch_video_patches(video_path, fps, patch_size, resolution)]
+        
+        total_step = patch_size // batch_size
+        if patch_size % batch_size != 0:
+            total_step += 1
+        
 
-#         return h
+        feats = []
+
+        with tqdm(total=patch_size, desc="Processed Patches") as pbar:
+
+            for start_idx in range(0, patch_size, batch_size):
+                end_idx = min(patch_size, start_idx + batch_size)
+
+                batch = torch.concat(patches[start_idx:end_idx], dim=0)
+                batch = batch.to("cuda", non_blocking=True).float().div_(255.0)
+                with torch.amp.autocast(device_type="cuda", dtype=torch.float16):
+                    feat = self.forward(batch)
+
+                feats.append(feat.detach().cpu().float())
+                pbar.update(end_idx-start_idx)
+
+                del batch, feat
+                torch.cuda.empty_cache()
+            
+            return torch.concat(feats, dim=0)
+
 
 def _probe_segment_length(video_path: str, target_fps: int, patch_size: int):
+    """
+    This method calculates and retunrs the length of segmments of patches.
+    """
 
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
@@ -112,14 +115,20 @@ def _probe_segment_length(video_path: str, target_fps: int, patch_size: int):
     return S
 
 def _num_clips_for_segment(S: int, clip_size: int, stride: int) -> int:
+    """
+    Calculates and returns number of clips for segments.
+    """
     S_eff = max(S, clip_size) 
     return (S_eff - clip_size) // stride + 1
 
-def _estimate_conv1_bytes(num_clips_in_batch: int, resize_dim: tuple,
+def _number_of_patch_bytes(num_clips_in_batch: int, resize_dim: tuple,
                            clip_size: int, dtype_bytes: int = 4,
-                           conv1_channels: int = 64) -> int:
+                           channels: int = 64) -> int:
+    """
+    Calculates and returns each patch's bytes
+    """
     H, W = resize_dim
-    bytes_per_clip = conv1_channels * clip_size * H * W * dtype_bytes
+    bytes_per_clip = channels * clip_size * H * W * dtype_bytes
     return bytes_per_clip * num_clips_in_batch
 
 @torch.no_grad()
@@ -132,6 +141,9 @@ def extract_C3D_features(extractor,
                           target_fps: int = 30,
                           safe_vram_gb: float = 3.0,
                           skip_log_path: str = None):
+    """
+    Extracts features from given video_paths and saves the features to given save_dir.
+    """
 
     os.makedirs(save_dir, exist_ok=True)
     if skip_log_path is None:
@@ -147,7 +159,7 @@ def extract_C3D_features(extractor,
     extractor.eval()
 
     safe_budget_bytes = safe_vram_gb * (1024 ** 3)
-    bytes_per_clip = _estimate_conv1_bytes(1, resize_dim, clip_size)
+    bytes_per_clip = _number_of_patch_bytes(1, resize_dim, clip_size)
 
     skipped = []
 
@@ -222,6 +234,6 @@ def extract_C3D_features(extractor,
 
 if __name__ == "__main__":
     data = torch.randn(1, 3, 54, 112, 112).to("cuda")
-    model = C3D_FeatureExtractor().to("cuda")
+    model = FeatureExtractor().to("cuda")
     output = model(data)
     print(output.shape)
