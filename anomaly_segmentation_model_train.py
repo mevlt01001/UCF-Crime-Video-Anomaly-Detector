46b5ded636f741231189a26810f1561c4b29b742
mevlt01001 <mevlutbasaran01@gmail.com>
2026-07-15 15:47:47 +0300

    Training optimized.

--- anomaly_segmentation_model_train.py
+++ anomaly_segmentation_model_train.py
@@ -1,10 +1,12 @@
-from utils import MILRankingNetwork, MIL_network_trainer
+import torch
+from utils import SegmentRankingModel, MIL_network_trainer
 
-model = MILRankingNetwork(input_dim=4096).to("cuda")
+model = SegmentRankingModel(input_dim=512).to("cuda")
 
 MIL_network_trainer(model=model,
-                    anormal_feat_dir="extracted_anomaly_features",
+                    anormal_feat_dir="extracted_anormal_features",
                     normal_feat_dir="extracted_normal_features",
-                    epochs=2000,
-                    batch_size=192,
-                    learning_rate=0.005)
+                    epochs=500,
+                    batch_size=128,
+                    learning_rate=0.001,
+                    test_ratio=0.1)
